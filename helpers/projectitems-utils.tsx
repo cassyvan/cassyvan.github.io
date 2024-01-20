26decad47ac433564b1c0b596771d082528da642
Cassy Van <cvan063@mtroyal.ca>
2024-01-20 14:08:02 -0700

    edit projects page to include vercel

--- helpers/projectitems-utils.tsx
+++ helpers/projectitems-utils.tsx
@@ -1,4 +1,6 @@
 const getProjectList = () => {
+  let portfolioSpanStyle =
+    "block bg-red-300 dark:bg-sky-200 inset-0 skew-y-2 relative inline-block";
   const myPortfolio = {
     projectName: "My Portfolio",
     imageUrl: "/images/portfolio.png",
@@ -6,13 +8,10 @@ const getProjectList = () => {
       <p key={1}>
         A place to feature some projects I have been working on, as well as a
         space to share some of my writing. Built with{" "}
-        <span className="block bg-red-300 dark:bg-sky-200 inset-0 skew-y-2 relative inline-block">
-          Next.js
-        </span>{" "}
-        and{" "}
-        <span className="block bg-red-300 dark:bg-sky-200 inset-0 skew-y-2 relative inline-block">
-          Tailwind CSS
-        </span>
+        <span className={portfolioSpanStyle}>Next.js</span> and{" "}
+        <span className={portfolioSpanStyle}>Tailwind CSS</span>
+        Auto deployments and hosting setup with{" "}
+        <span className={portfolioSpanStyle}>Vercel</span>
       </p>,
     ],
     projectDemo: "",

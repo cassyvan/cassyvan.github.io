439154474c8a768b8c51933ba9e042c9cf64c624
Cassy Van <cvan063@mtroyal.ca>
2024-03-06 19:53:40 -0700

    added journal app to project list

--- helpers/projectitems-utils.tsx
+++ helpers/projectitems-utils.tsx
@@ -6,8 +6,7 @@ const getProjectList = () => {
     imageUrl: "/images/portfolio.png",
     projectDesc: [
       <p key={1}>
-        A place to feature some projects I have been working on, as well as a
-        space to share some of my writing. Built with{" "}
+        A place to feature some projects I have been working on. Built with{" "}
         <span className={portfolioSpanStyle}>Next.js</span> and{" "}
         <span className={portfolioSpanStyle}>Tailwind CSS</span>
         Auto deployments and hosting setup with{" "}
@@ -19,7 +18,35 @@ const getProjectList = () => {
     key: 1,
   };
 
-  return [myPortfolio];
+  const myJournal = {
+    projectName: "My Journal",
+    imageUrl: "/images/journal.png",
+    projectDesc: [
+      <div key={2}>
+        <p>
+          A journaling app to help you keep all your thoughts, or notes, or
+          whatever you like in one space. Built with{" "}
+          <span className={portfolioSpanStyle}>Next.js</span>,{" "}
+          <span className={portfolioSpanStyle}>Firebase</span> and{" "}
+          <span className={portfolioSpanStyle}>Tailwind CSS</span>
+          Auto deployments and hosting setup with{" "}
+          <span className={portfolioSpanStyle}>Vercel</span>
+        </p>
+        <br />
+        <p>
+          For demo purposes, if you do not wish to make an account, then please
+          use the following:{" "}
+        </p>
+        <p>Email: test@myjournal.com</p>
+        <p>Password: test123</p>
+      </div>,
+    ],
+    projectDemo: "https://cassysjournal.vercel.app/",
+    projectGitHub: "https://github.com/cassyvan/myjournal",
+    key: 2,
+  };
+
+  return [myPortfolio, myJournal];
 };
 
 export default getProjectList;
